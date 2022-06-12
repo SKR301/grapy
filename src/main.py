2af7e19b9b14bbf4b6c21765f75431c8ce8122d9
SKR301 <chunnu.bihat@gmail.com>
2022-06-12 22:00:21 +0530

    export button

--- src/main.py
+++ src/main.py
@@ -4,9 +4,15 @@ root = Tk()
 root.state('zoomed')
 root.title('grapy')
 
+# func
+def exportPoints():
+    tempLabel.config(text='Exporting...')
+    
+
+# main
 if __name__ == '__main__':
-    topBarFrame = Frame(root, bd=5)
-    topBarFrame.grid(row=0, column=0)
+    # topBarFrame = Frame(root, bd=5)
+    # topBarFrame.grid(row=0, column=0)
 
     graphFrame = Frame(root, bd=5)
     graphFrame.grid(row=1, column=0)
@@ -16,8 +22,11 @@ if __name__ == '__main__':
 
     menuBarFrame = Frame(root, bd=5)
     menuBarFrame.grid(row=1, column=1)
-    
-    exportBtn = Button(root, text = 'Export')
+
+    tempLabel = Label(root, text='SKRinternationals 2022')
+    tempLabel.grid(row=2, column=0)
+
+    exportBtn = Button(root, text = 'Export', command=exportPoints, padx=150, bg='#0078d7', fg='white')
     exportBtn.grid(row=2, column=1)
 
     mainloop()
