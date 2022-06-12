ff5f516a490243bc462c92108f53e4708a4fdbac
SKR301 <chunnu.bihat@gmail.com>
2022-06-13 00:17:21 +0530

    undo a plotted point

--- src/main.py
+++ src/main.py
@@ -24,6 +24,13 @@ def plotPoint(event):
     points.append([x, y])
     tempLabel.config(text=f'Plot at {x}, {y}')
 
+def undoPlotPoint(event):
+    if len(points) < 1:
+        return
+
+    lastPoint = points.pop(-1)
+    x, y = lastPoint[0]+2, -1 * (lastPoint[1]-1002)
+    graph.create_oval(x-3, y-3, x+3, y+3, width = 0, fill = 'white')
 
 # if __name__ == '__main__': ----------------------------------------------------------------------------------------
     # topBarFrame = Frame(root, bd=5)
@@ -34,6 +41,7 @@ graphFrame.grid(row=1, column=0)
 
 graph = Canvas(graphFrame, bg='white', height=1000, width=1500)
 graph.bind('<Button-1>', plotPoint)
+graph.bind_all('<Control-z>', undoPlotPoint)
 graph.grid(row=0, column=0)
 
 menuBarFrame = Frame(root, bd=5)
