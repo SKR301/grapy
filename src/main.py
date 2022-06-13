15d80dc431d81232c1a5186cdd3682f5c99e5432
SKR301 <chunnu.bihat@gmail.com>
2022-06-13 22:07:32 +0530

    added slider for linear point plotting

--- src/main.py
+++ src/main.py
@@ -6,6 +6,9 @@ root.state('zoomed')
 root.title('grapy')
 
 points = []
+linRegSlope = DoubleVar()
+linRegConstant = DoubleVar()
+linRegSpread = DoubleVar()
 
 # func -------------------------------------------------------------------------------------------------------------
 def exportPoints():
@@ -32,9 +35,28 @@ def undoPlotPoint(event):
     x, y = lastPoint[0]+2, -1 * (lastPoint[1]-1002)
     graph.create_oval(x-3, y-3, x+3, y+3, width = 0, fill = 'white')
 
+def hideLinRegOpt():
+    linRegSlopeScale.grid_remove()
+    linRegConstantScale.grid_remove()
+    linRegSpreadScale.grid_remove()
+    linRegBtn.config(command=showLinRegOpt)
+
+def showLinRegOpt():
+    linRegSlopeScale.grid(row=0, column=0)
+    linRegConstantScale.grid(row=1, column=0)
+    linRegSpreadScale.grid(row=2, column=0)
+    linRegBtn.config(command=hideLinRegOpt)
+
+def plotLinearRegPoints(event):
+    tempLabel.config(text=f'{linRegSlope.get()}, {linRegConstant.get()}, {linRegSpread.get()}')
+
+def showLogRegOpt():
+    print('show logistic regression')
+
+
 # if __name__ == '__main__': ----------------------------------------------------------------------------------------
-    # topBarFrame = Frame(root, bd=5)
-    # topBarFrame.grid(row=0, column=0)
+topBarFrame = Frame(root, bd=5)
+topBarFrame.grid(row=0, column=0)
 
 graphFrame = Frame(root, bd=5)
 graphFrame.grid(row=1, column=0)
@@ -44,13 +66,31 @@ graph.bind('<Button-1>', plotPoint)
 graph.bind_all('<Control-z>', undoPlotPoint)
 graph.grid(row=0, column=0)
 
+
 menuBarFrame = Frame(root, bd=5)
 menuBarFrame.grid(row=1, column=1)
+
+linRegBtn = Button(menuBarFrame, text='Linear Reg', command=showLinRegOpt)
+linRegBtn.grid(row=0, column=0)
+
+
+linRegFrame = Frame(menuBarFrame, bd=5)
+linRegFrame.grid(row=1, column=0)
+
+linRegSlopeScale = Scale(linRegFrame, from_=0, to=90, orient=HORIZONTAL, length=300, variable=linRegSlope, command=plotLinearRegPoints)
+linRegConstantScale = Scale(linRegFrame, from_=-1500, to=1500, orient=HORIZONTAL, length=300, variable=linRegConstant, command=plotLinearRegPoints)
+linRegSpreadScale = Scale(linRegFrame, from_=0, to=100, orient=HORIZONTAL, length=300, variable=linRegSpread, command=plotLinearRegPoints)
+
+
+# logRegnBtn = Button(menuBarFrame, text='Logistic Reg', command=showLogRegOpt)
+# logRegnBtn.grid(row=1, column=0)
+
 
 tempLabel = Label(root, text='SKRinternationals 2022')
 tempLabel.grid(row=2, column=0)
 
-exportBtn = Button(root, text = 'Export', command=exportPoints, padx=150, bg='#0078d7', fg='white')
+
+exportBtn = Button(root, text='Export', command=exportPoints, padx=150, bg='#0078d7', fg='white')
 exportBtn.grid(row=2, column=1)
 
 mainloop()
